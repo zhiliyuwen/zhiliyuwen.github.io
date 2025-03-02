e965a739018b8fdb762f2be30ca6c644f91051ac
zhiliyuwen <zhiliyuwen@outlook.com>
2025-03-02 11:35:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function checkPassword() {
     if (password === correctPassword) {
         // 密钥正确，跳转到网站主页面
         localStorage.setItem('isAuthenticated', 'true');
-        window.location.href = 'docs/index.html'; // 替换为你网站的主页面
+        window.location.href = 'index.html'; // 替换为你网站的主页面
     } else {
         // 密钥错误，显示错误信息
         errorMessage.style.display = 'block';
